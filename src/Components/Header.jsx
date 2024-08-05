4825992b1a4b39ff16d326b2f716cdb1134ab56b
TejalZiman24 <tejalziman42@gmail.com>
2024-08-05 22:47:34 +0530

    Modified Sidebar

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,32 +1,37 @@
-import React from 'react'
-import './Header.css'
-import {BsFillBellFill, BsFillEnvelopeFill, BsPersonCircle, BsSearch, BsJustify} from 'react-icons/bs'
+import React from "react";
+import "./Header.css";
+import {
+  BsFillBellFill,
+  BsFillEnvelopeFill,
+  BsPersonCircle,
+  BsSearch,
+  BsJustify,
+} from "react-icons/bs";
 import { FaToggleOff } from "react-icons/fa6";
-
 
 const Header = (props) => {
   return (
     <header className={`header ${props.mode}`}>
-      
-    <div className='menu-icon' >
-       <BsJustify className='icon' />
-    </div>
-    <div className='header-left'>
-      
-       <BsSearch className='icon'/>
-       <input className='search-text' type="search"placeholder="Search..." aria-label="Search"/>
-       
-    </div>
-    <div className='header-right'>
-    
-     <BsFillBellFill className='icon'/>
-     <BsFillEnvelopeFill className='icon'/>
-     <BsPersonCircle className='icon'/>
-     <FaToggleOff className='icon' onClick={props.togglemode}/>
-     </div>
-    
-   </header>
-  )
-}
+      <div className="menu-icon">
+        <BsJustify className="icon" />
+      </div>
+      <div className="header-left">
+        <BsSearch className="icon" />
+        <input
+          className="search-text"
+          type="search"
+          placeholder="Search..."
+          aria-label="Search"
+        />
+      </div>
+      <div className="header-right">
+        <BsFillBellFill className="icon" />
+        <BsFillEnvelopeFill className="icon" />
+        <BsPersonCircle className="icon" />
+        <FaToggleOff className="icon" onClick={props.togglemode} />
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,58 +1,56 @@
 import React, { useState } from "react";
 import "./Sidebar.css";
-import {
-  BsCart3,
-  BsList,
-  BsGrid1X2Fill,
-  BsFillGrid3X3GapFill,
-  BsPeopleFill,
-} from "react-icons/bs";
+
+import Hamburger from "hamburger-react";
+import { BsList } from "react-icons/bs";
+import { AiOutlineAppstoreAdd, AiOutlineHome } from "react-icons/ai";
+import { BiBarChartSquare } from "react-icons/bi";
+import { TbChecklist } from "react-icons/tb";
+import { LiaWalletSolid } from "react-icons/lia";
 
 const Sidebar = () => {
-  const[navOpen, setNavOpen]=useState(true)
-  const navToggle=()=>{
-    setNavOpen(!navOpen)
-  }
+  const [navOpen, setNavOpen] = useState(true);
+  const navToggle = () => {
+    setNavOpen(!navOpen);
+  };
   return (
-    
-    <aside id="sidebar" style={{width: navOpen ? "258px" : "80px"}}>
-      <div className="sidebar-title"  style={{display: navOpen ? "block" : "0px"}}>
-        <div className="sidebar-brand"  >
-          ADMIN
-          <span style={{display:"inline-block",width:"30px"}}></span>
-          <BsList onClick={navToggle} />
+    <aside id="sidebar">
+      <div className="sidebar-title">
+        <div className="sidebar-brand">
+          <Hamburger size={22} toggled={navOpen} toggle={navToggle} />
+          <span style={{ display: "inline-block", width: "10px" }}></span>
+          <span>ADMIN</span>
         </div>
-       
       </div>
 
-      <ul className="sidebar-list" >
+      <ul className="sidebar-list">
         <li className="sidebar-list-item">
-          <a href="">
-            <BsGrid1X2Fill className="icon"  /> Home
+          <a href="" className="navbar-items">
+            <AiOutlineHome className="icon" /> <span>Home</span>
           </a>
         </li>
 
         <li className="sidebar-list-item">
-          <a href="">
-            <BsCart3 className="icon" /> Data
+          <a href="" className="navbar-items">
+            <BiBarChartSquare className="icon" /> <span>Analytics</span>
           </a>
         </li>
 
         <li className="sidebar-list-item">
-          <a href="">
-            <BsCart3 className="icon" /> TODO List
+          <a href="" className="navbar-items">
+            <TbChecklist className="icon" /> <span>Reminders</span>
           </a>
         </li>
 
         <li className="sidebar-list-item">
-          <a href="">
-            <BsFillGrid3X3GapFill className="icon" /> Wallet
+          <a href="" className="navbar-items">
+            <LiaWalletSolid className="icon" /> <span>Wallet</span>
           </a>
         </li>
 
         <li className="sidebar-list-item">
-          <a href="">
-            <BsPeopleFill className="icon" /> Store
+          <a href="" className="navbar-items">
+            <AiOutlineAppstoreAdd className="icon" /> <span>Store</span>
           </a>
         </li>
       </ul>
